3b68f9a0a5fa76f6d54050afb647fcdcf090e350
Aleksandar <futavcina@gmail.com>
2017-10-02 20:01:41 +0200

    Uradjena klasa Roba

--- src/main/java/model/Roba.java
+++ src/main/java/model/Roba.java
@@ -1,14 +1,84 @@
 package model;
 
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "roba")
 public class Roba {
 
+
+    @DatabaseField(generatedId = true)
     private int id;
+
+    @DatabaseField(columnName = POLJE_NAZIV, canBeNull = false)
     private String naziv;
+
+    @DatabaseField(columnName = POLJE_OPIS, canBeNull = false)
     private String opis;
+
+    @DatabaseField(columnName = POLJE_TEZINA, canBeNull = false)
     private double tezina;
 
-    public static String POLJE_NAZIV = "naziv";
-    public static String POLJE_OPIS = "opis";
-    public static String POLJE_TEZINA = "tezina";
 
+    public static final String POLJE_NAZIV = "naziv";
+    public static final String POLJE_OPIS = "opis";
+    public static final String POLJE_TEZINA = "tezina";
+
+    @ForeignCollectionField(foreignFieldName = "roba")
+    private ForeignCollection<Roba> aviona;
+
+    public Roba(){
+
+    }
+
+    public Roba(String naziv, String opis, double tezina){
+        this.naziv = naziv;
+        this.opis = opis;
+        this.tezina = tezina;
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+
+    @Override
+    public String toString(){
+        return "Avion{" +
+                "id=" + id +
+                ", naziv=" + naziv +
+                ", opis=" + opis +
+                ", tezina=" + tezina + "}";
+    }
 }
