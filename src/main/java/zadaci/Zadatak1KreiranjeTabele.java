1ae628cb3b26ddf2be723065dc25ff393fe96498
Aleksandar <futavcina@gmail.com>
2017-10-02 20:13:18 +0200

    Uradjen zadatak kreiranja tabela

--- src/main/java/zadaci/Zadatak1KreiranjeTabele.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabele.java
@@ -2,6 +2,9 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
 
@@ -12,6 +15,14 @@ public class Zadatak1KreiranjeTabele {
 
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+
+            TableUtils.dropTable(connectionSource, Avion.class,true);
+            TableUtils.dropTable(connectionSource, Roba.class, true);
+
+            TableUtils.createTable(connectionSource, Avion.class);
+            TableUtils.createTable(connectionSource, Roba.class);
+
+
         }catch (Exception e){
             e.printStackTrace();
         }finally {
