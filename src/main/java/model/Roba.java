fff60fa3cf3dd97d3f36318a5226cc6a54a5cc91
Aleksandar <futavcina@gmail.com>
2017-10-02 19:51:58 +0200

    Uradjena klasa Avion

---
+++ src/main/java/model/Roba.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Roba {
+
+    private int id;
+    private String naziv;
+    private String opis;
+    private double tezina;
+
+    public static String POLJE_NAZIV = "naziv";
+    public static String POLJE_OPIS = "opis";
+    public static String POLJE_TEZINA = "tezina";
+
+}
